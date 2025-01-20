adc9077dcb6e91f1263ae06313a7c7c69226aa2b
Gina <90372670+Gnblink0@users.noreply.github.com>
2025-01-20 15:00:04 -0800

    Revert "Week3"

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,13 +1,12 @@
-import { Button, StyleSheet, Text, TextInput, View, Modal } from 'react-native'
+import { StyleSheet, Text, TextInput, View } from 'react-native'
 import React, { useState } from 'react'
 
 interface InputProps {
   autoFocus?: boolean;
-  InputHandler: (text: string) => void;
-  visible: boolean;
 }
   
-export default function Input({ autoFocus, InputHandler, visible }: InputProps) {
+export default function Input({ autoFocus }: InputProps) {
+
   const [text, setText] = useState('');
   const [charCount, setCharCount] = useState(0);
   const [isFocused, setIsFocused] = useState(true);
@@ -17,48 +16,27 @@ export default function Input({ autoFocus, InputHandler, visible }: InputProps) 
     setCharCount(text.length);
   };
   
-  const handleConfirm = () => {
-    InputHandler(text);
-    setCharCount(0);
-  };
-  
   return (
-    <Modal 
-      visible={visible}
-      animationType="slide"
-    >
-      <View style={styles.container}>
-        <TextInput
-          placeholder="Please type something"
-          onChangeText={handleTextChange}
-          autoFocus={autoFocus} 
-          onFocus={() => setIsFocused(true)}
-          onBlur={() => setIsFocused(false)}
-        />
-        
-        {isFocused && charCount > 0 && <Text>Character Count: {charCount}</Text>}
+    <View>
+      <TextInput
+        placeholder="Please type something"
+        onChangeText={handleTextChange}
+        autoFocus={autoFocus} 
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+      />
+      
+      {isFocused && charCount > 0 && <Text>Character Count: {charCount}</Text>}
 
-        {!isFocused && (
-          <Text>
-            {text.length >=  3
-            ? "Thank you"
-            :"Please type more than 3 characters"}
-          </Text>
-        )}
-        <Button 
-          title="Confirm" 
-          onPress={handleConfirm}
-        />
-      </View>
-    </Modal>
+      {!isFocused && (
+        <Text>
+          {text.length >=  3
+          ? "Thank you"
+          :"Please type more than 3 characters"}
+        </Text>
+      )}
+    </View>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-});
+const styles = StyleSheet.create({})
