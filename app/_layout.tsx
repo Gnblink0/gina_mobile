cc5a348bfe9f40cf8d269e7ba429de1db6f4c32b
gnblink0 <gnblink44@gmail.com>
2025-03-10 14:37:06 -0700

    feat: Implement Firebase authentication routing and loading state

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,22 +3,42 @@ import { useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "@/Firebase/firebaseSetup";
 import { ActivityIndicator, View } from "react-native";
+import { router, Slot } from "expo-router";
 
 export default function RootLayout() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // Temporary mock authentication
-    setIsAuthenticated(false);
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        setUserLoggedIn(true);
+      } else {
+        // User is signed out
+        setUserLoggedIn(false);
+      }
+      setIsLoading(false);
+    });
+    return () => unsubscribe();
   }, []);
 
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      {!isAuthenticated ? (
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      ) : (
-        <Stack.Screen name="(protected)" options={{ headerShown: false }} />
-      )}
-    </Stack>
-  );
+  useEffect(() => {
+    if (userLoggedIn) {
+      router.replace("/(protected)/");
+    } else if (!userLoggedIn) {
+      router.replace("/(auth)/login");
+    }
+  }, [userLoggedIn]);
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color="#6200ee" />
+      </View>
+    );
+  }
+
+  return <Slot />;
 }
